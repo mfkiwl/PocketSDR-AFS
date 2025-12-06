35935c6a7eb4b313c25c31c0176dc336b5db3de5
OSQZSS <osqzss@gmail.com>
2025-12-06 13:38:21 +0900

    Update pocket_trk.sh

--- app/pocket_trk/pocket_trk.sh
+++ app/pocket_trk/pocket_trk.sh
@@ -2,7 +2,8 @@
 
 if [ $# -ne 1 ]; then
     echo "No baseband file is specified. Open frontend device."
-    ./pocket_trk -sig AFSD -prn 2-8 -sig AFSP -prn 2-8 -InQ -c ../../conf/pocket_L1L5_20MHz.conf 
+#    ./pocket_trk -sig AFSD -prn 2-8 -sig AFSP -prn 2-8 -InQ -c ../../conf/pocket_L1L5_20MHz.conf 
+    ./pocket_trk -sig AFSD -prn 2-8 -sig AFSP -prn 2-8 -log log.txt
 else
     ./pocket_trk -sig AFSD -prn 2-8 -sig AFSP -prn 2-8 -f 12 -IQ 2 -log log.txt $1 
 fi
