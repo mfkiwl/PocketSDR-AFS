940f0c65f9c0080fc0ff49e94c4a93a1b9340528
OSQZSS <osqzss@gmail.com>
2026-04-12 20:13:19 +0900

    Support SoapySDR

--- app/pocket_trk/pocket_trk.sh
+++ app/pocket_trk/pocket_trk.sh
@@ -2,7 +2,8 @@
 
 if [ $# -ne 1 ]; then
     echo "No baseband file is specified. Open frontend device."
-    ./pocket_trk -sig AFSD -prn 2-8 -sig AFSP -prn 2-8 -InQ -c ../../conf/pocket_L1L5_20MHz.conf 
+#    ./pocket_trk -sig AFSD -prn 2-8 -sig AFSP -prn 2-8 -InQ -c ../../conf/pocket_L1L5_20MHz.conf 
+    ./pocket_trk -sig AFSD -prn 2-8 -sig AFSP -prn 2-8 -f 12 -IQ 2 -fmt CS16 
 else
     ./pocket_trk -sig AFSD -prn 2-8 -sig AFSP -prn 2-8 -f 12 -IQ 2 -log log.txt $1 
 fi
